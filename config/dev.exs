ede4e3a46ac9e19fb7f89410243f964805d6120b
Andreas Eriksson <andreas@codered.se>
2020-07-21 09:18:45 +0200

    Update config to run on port 4020

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :graphql_tutorial, GraphqlTutorial.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :graphql_tutorial, GraphqlTutorialWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4020],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
